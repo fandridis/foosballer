af43031d43156ccd88e8dd86de45c0f17d11c435
George Fandridis <gfa@optimise-international.com>
2018-11-30 13:59:58 +0100

    Add reset password page

--- src/pages/ResetPassword/index.js
+++ src/pages/ResetPassword/index.js
@@ -1,15 +1,59 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from "react-router-dom";
 
-import PasswordReset from '../../components/PasswordReset';
+import { withFirebase } from '../../hocs/Firebase';
 import * as ROUTES from '../../routes/names';
 
-class Login extends Component {
-  render(props) {
+class ResetPassword extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      error: null
+    };
+  }
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  onSubmit = async event => {
+    event.preventDefault();
+    const { email } = this.state;
+
+    try {
+      await this.props.firebase.doPasswordReset(email);
+      this.setState({ email: '', error: null });
+
+    } catch (error) {
+      this.setState({ error });
+    }
+  };
+
+
+  render() {
+    const { email, error } = this.state;
+
+    const isInvalid = email === '';
+
     return (
       <Fragment>
-        <h3>Reset password</h3>
-        <PasswordReset />
+        <h3>Reset Password</h3>
+        <form onSubmit={this.onSubmit}>
+          <input
+            name="email"
+            value={this.state.email}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Email Address"
+          />
+          <button disabled={isInvalid} type="submit">
+            Reset My Password
+        </button>
+
+          {error && <p>{error.message}</p>}
+        </form>
 
         <p><Link to={ROUTES.LOGIN}>Back</Link></p>
       </Fragment>
@@ -17,4 +61,7 @@ class Login extends Component {
   }
 }
 
-export default Login;
+export default withFirebase(ResetPassword);
+
+
+
