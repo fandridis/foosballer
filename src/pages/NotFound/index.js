b5fa7c440a5d7c4116875c76eb8ea9d0121f13c4
Gabriella G. R <g.rozenaite@gmail.com>
2018-12-04 21:42:45 +0100

    Add FontAwesome

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 
 const NotFound = () => {
   return (
     <div>
       <h1>Page not found</h1>
+      <FontAwesomeIcon icon={faCoffee} />
     </div>
   );
 };
