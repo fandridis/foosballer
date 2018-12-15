536efa9e149724169d4eb490b527ff06c4b52f3c
Gabriella G. R <g.rozenaite@gmail.com>
2018-12-15 23:44:49 +0100

    Add loading

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 import { colors } from '../../css/Variables';
+import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Graphics from '../../assets/images/loading.png'
 
 const Wrapper = styled.div`
 width: 100vw;
 height: 100vh;
+
 display: flex;
+flex-direction: column;
 justify-content: center;
 align-items: center;
-background-color: ${colors.normal.lightText}
-color: red;
+
+color: ${colors.normal.greyText};
+
+background-image: url("src/assets/images/background.png")
 `
+const randomText = [
+  "Just one more second..", 
+  "Still loading..",
+  "Keeps loading...",
+  "..."
+];
+const GetRandomTextMethod = () => {
+   return randomText[Math.floor(Math.random()*randomText.length)];
+}
 
 const Loading = () => {
   return (
     <Wrapper>
-      <FontAwesomeIcon icon='trash-alt' size='4x' color={colors.normal.primary} pulse />
+      {/* <img className="PageGraphics" src={Graphics} alt="graphics"></img> */}
+      <FontAwesomeIcon id="LoaderIcon" icon='futbol' size='4x' color={colors.normal.primary}/>
+      <h2>Loading...</h2>
+      <h2 className="Loading-text">{GetRandomTextMethod()}</h2>
     </Wrapper>
   );
 };
